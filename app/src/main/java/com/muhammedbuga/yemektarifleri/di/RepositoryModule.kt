39ce58fcf48e1e56f057562e763b1b1ec888f3fe
Muhammed Buğa <70836304+muhorinho@users.noreply.github.com>
2024-06-12 13:26:39 +0300

    Update RepositoryModule.kt

--- app/src/main/java/com/muhammedbuga/yemektarifleri/di/RepositoryModule.kt
+++ app/src/main/java/com/muhammedbuga/yemektarifleri/di/RepositoryModule.kt
@@ -1,11 +1,14 @@
 package com.muhammedbuga.yemektarifleri.di
+import android.content.Context
 import com.muhammedbuga.yemektarifleri.data.local.RecipeDao
 import com.muhammedbuga.yemektarifleri.data.remote.RecipeApi
 import com.muhammedbuga.yemektarifleri.data.repository.RecipeRepositoryImpl
 import com.muhammedbuga.yemektarifleri.domain.repository.RecipeRepository
+import com.muhammedbuga.yemektarifleri.util.NetworkUtil
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -15,10 +18,17 @@ object RepositoryModule {
 
     @Provides
     @Singleton
+    fun provideNetworkUtil(@ApplicationContext context: Context): NetworkUtil {
+        return NetworkUtil(context)
+    }
+
+    @Provides
+    @Singleton
     fun provideRecipeRepository(
         recipeDao: RecipeDao,
-        recipeApi: RecipeApi
+        recipeApi: RecipeApi,
+        networkUtil: NetworkUtil
     ): RecipeRepository {
-        return RecipeRepositoryImpl(recipeDao, recipeApi)
+        return RecipeRepositoryImpl(recipeDao, recipeApi, networkUtil)
     }
 }
